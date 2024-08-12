4e745b18ffa5f796da4f44e0887b5d7b806c5587
Hanh-Nguyen-Tamron <hanhntt85vp@gmail.com>
2024-08-12 18:04:39 +0700

    Update tinhtiendien.py

--- tinhtiendien.py
+++ tinhtiendien.py
@@ -5,8 +5,6 @@
 # Bậc 5: 301 - 400 kWh tiếp theo, giá 2.834 VND/kWh.
 # Bậc 6: Trên 400 kWh, giá 2.927 VND/kWh.
 
-sodientieuthu = float(input('Mời bạn nhập số điện đã tiêu thụ'))
-
 bac1=1.678
 bac2 =1.734
 bac3=2.014
@@ -14,18 +12,20 @@ bac4=2.536
 bac5=2.834
 bac6=2.927
 
-if(sodientieuthu<=50):
+sodientieuthu = float(input("Nhập số điện tiêu thụ: "))
+
+if sodientieuthu<=50:
     sotienphaitra=sodientieuthu *bac1
-elif(sodientieuthu<=100):
-    sotienphaitra= 50*bac1 +(sodientieuthu-50)*bac2
-elif(sodientieuthu<=200):
-    sotienphaitra = 50*bac1 +50*bac2 + (sodientieuthu-100)*bac3
-elif(sodientieuthu<=300):
-    sotienphaitra= 50*bac1 + 50 *bac2 + 100*bac3 +(sodientieuthu-200)*bac4
-elif(sodientieuthu<=400):
-    sotienphaitra=50*bac1 +50*bac2+100*bac3 +100*bac4 +(sodientieuthu-300)*bac5
-elif(sodientieuthu>400):
-    sotienphaitra=50*bac1 +50*bac2+100*bac3 +100*bac4 +100*bac5 + (sodientieuthu-400)*bac6
+elif sodientieuthu<=100:
+    sotienphaitra= 50 * bac1 + (sodientieuthu-50) * bac2
+elif sodientieuthu<=200):
+    sotienphaitra = 50 * bac1 + 50 * bac2 + (sodientieuthu-100) * bac3
+elif sodientieuthu<=300):
+    sotienphaitra= 50 * bac1 + 50 * bac2 + 100 * bac3 +(sodientieuthu-200) * bac4
+elif sodientieuthu<=400):
+    sotienphaitra=50 * bac1 + 50 * bac2 + 100 * bac3 + 100 * bac4 +(sodientieuthu-300) * bac5
+elif sodientieuthu>400):
+    sotienphaitra=50 * bac1 + 50 * bac2 + 100 * bac3 + 100 * bac4 + 100 * bac5 + (sodientieuthu-400) * bac6
 else:
-    print('Số điện không hợp lệ')
+    print("Không hợp lệ")
 print("Số tiền phải trả là : %2.f"%sotienphaitra)
